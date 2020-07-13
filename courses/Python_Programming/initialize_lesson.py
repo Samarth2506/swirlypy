3dfcdc313f7ef0c567edb8e8d796b3649afb3e5f
Samarth <samarthmc@gmail.com>
2020-07-12 23:16:33 -0400

    added lists lesson in Python Programming

--- courses/Python_Programming/initialize_lesson.py
+++ courses/Python_Programming/initialize_lesson.py
@@ -2,4 +2,5 @@
 # Users creating lessons should add required variables to the data dict using get_data() function below. 
 def get_data():
     data = {}
+    data['cubes'] = [1, 8, 27, 65, 125]
     return data
