8c110dd1fe5f42980d674fe29e7056f5cfea1593
Samarth <samarthmc@gmail.com>
2020-08-30 22:45:09 -0400

    Added lesson to Python Programming

--- courses/Demo_Course/scripts/boring_function.py
+++ courses/Demo_Course/scripts/boring_function.py
@@ -1,1 +1,3 @@
 # Please complete the boring function below. Save and close when done!
+def boring_function():
+    return 'random'
--- courses/Python_Programming/initialize_lesson.py
+++ courses/Python_Programming/initialize_lesson.py
@@ -4,4 +4,6 @@ def get_data():
     data = {}
     data['cubes'] = [1, 8, 27, 65, 125]
     data['words'] = ['cat', 'window', 'defenestrate']
+    data['t'] = 12345, 54321, 'hello!'
+    data['sample_list'] = ['apple', 'orange', 'apple', 'pear', 'orange', 'banana']
     return data
