b1a9205f1503dd966df46d0487e4707d7d3b609b
Samarth <samarthmc@gmail.com>
2020-08-31 21:31:35 -0400

    changed setup file to publish on pypi

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ try:
     version = subprocess.check_output(["git", "describe", "--dirty=+"],
             universal_newlines=True)
 except subprocess.CalledProcessError as e:
-    version = "0.0.12"
+    version = "0.0.1"
 print(version)
 
 setup(name='swirlypy',
@@ -18,6 +18,7 @@ setup(name='swirlypy',
         author_email='samarthmc@gmail.com',
         url='https://github.com/Samarth2506/swirlypy',
         include_package_data=True,
+        download_url = 'https://github.com/Samarth2506/swirlypy/archive/0.0.1.tar.gz',
         scripts=['swirlypy/run_swirlypy'],
         packages=['swirlypy', 'swirlypy.questions','courses'],
         install_requires=['pyyaml','numpy', 'pandas', 'sklearn','tqdm'],
